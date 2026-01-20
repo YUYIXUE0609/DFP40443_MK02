e0e6b0377975c3a30274d272186c6a3b3175b097
YUYIXUE0609 <johnnyyu0926@gmail.com>
2026-01-20 14:19:49 +0800

    updated index.php, and add README.md

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Profil Saya</title>
-    <link rel="stylesheet" href="style.css"> <!-- 引用 CSS 文件 -->
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Profil Diri</h1>
 
-    <?php include 'biodata.php'; ?>  <!-- 引用 PHP 文件 -->
+    <?php include 'biodata.php'; ?>  
 
     <p>Nama: <?= $nama ?></p>
     <p>No. Matrik: <?= $nomatrik ?></p>
