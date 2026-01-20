6990b4773cd79544cf5ee737884f722f22c68185
YUYIXUE0609 <johnnyyu0926@gmail.com>
2026-01-20 10:39:13 +0800

    split index.php into index,style,biodata

---
+++ biodata.php
@@ -0,0 +1,5 @@
+<?php
+$nama = "YU YI XUE";
+$nomatrik = "18DIT24F1116";
+$kelas = "DIT4B";
+?>
--- index.php
+++ index.php
@@ -1,17 +1,15 @@
-<?php
-$nama = "YU YI XUE";
-$nomatrik = "18DIT24F1116";
-$kelas = "DIT4B";
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Profil Saya</title>
+    <link rel="stylesheet" href="style.css"> <!-- 引用 CSS 文件 -->
 </head>
 <body>
     <h1>Profil Diri</h1>
-    
+
+    <?php include 'biodata.php'; ?>  <!-- 引用 PHP 文件 -->
+
     <p>Nama: <?= $nama ?></p>
     <p>No. Matrik: <?= $nomatrik ?></p>
     <p>Kelas: <?= $kelas ?></p>
@@ -19,3 +17,4 @@ $kelas = "DIT4B";
     <img src="profile.png" alt="Foto Saya" width="200">
 </body>
 </html>
+
