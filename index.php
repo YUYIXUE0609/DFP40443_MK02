d77e4c57fb57b970c458181906b084d5bbd143d8
YUYIXUE0609 <johnnyyu0926@gmail.com>
2026-01-20 09:29:13 +0800

    DIT4B_18DIT24F1116_YU_YI_XUE

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$nama = "YU YI XUE";
+$nomatrik = "18DIT24F1116";
+$kelas = "DIT4B";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil Saya</title>
+</head>
+<body>
+    <h1>Profil Diri</h1>
+    
+    <p>Nama: <?= $nama ?></p>
+    <p>No. Matrik: <?= $nomatrik ?></p>
+    <p>Kelas: <?= $kelas ?></p>
+
+    <img src="profile.png" alt="Foto Saya" width="200">
+</body>
+</html>
